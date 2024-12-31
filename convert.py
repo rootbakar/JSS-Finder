7af4c8be0aad376d9a3e2265b9f335a0700aea2c
rootbakar <43517550+rootbakar@users.noreply.github.com>
2024-12-31 07:58:44 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,19 +1,14 @@
-# Skrip Python untuk memproses file list-js.txt
-
 def extract_links(input_file, output_file):
     try:
-        # Buka file input untuk dibaca
         with open(input_file, 'r') as infile:
             lines = infile.readlines()
 
-        # Ekstraksi link valid
         links = []
         for line in lines:
             if line.startswith('[+] Valid URL found:'):
                 link = line.split(':', 1)[1].strip()
                 links.append(link)
 
-        # Simpan hasil ke file output
         with open(output_file, 'w') as outfile:
             outfile.write('\n'.join(links))
 
@@ -23,9 +18,8 @@ def extract_links(input_file, output_file):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Nama file input dan output
-input_file = 'list-js.txt'  # Ganti dengan nama file Anda
-output_file = 'extracted-links.txt'  # Nama file hasil output
+input_file = 'list-js.txt'
+output_file = 'extracted-links.txt'
 
 # Jalankan fungsi ekstraksi
 extract_links(input_file, output_file)
